289f40a6c179782924db551b9df06b2457f06a19
Hung <vuhuyhung2792000@gmail.com>
2022-07-16 15:33:11 +0700

    lab04

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -11,12 +11,6 @@ import {
 export default class Menu extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      selectedDish: null,
-    };
-  }
-  onDishSelect(dish) {
-    this.setState({ selectedDish: dish });
   }
 
   renderDish(dish) {
@@ -37,7 +31,7 @@ export default class Menu extends React.Component {
     const menu = this.props.dishes.map((dish) => {
       return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
+          <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
               <CardTitle>{dish.name}</CardTitle>
@@ -51,7 +45,7 @@ export default class Menu extends React.Component {
         <div className="row">{menu}</div>
         <div className="row">
           <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.state.selectedDish)}
+            {/* {this.renderDish(this.state.selectedDish)} */}
           </div>
         </div>
       </div>
